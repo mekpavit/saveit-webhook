540b0babdbfa0ce1ec68c645dae0c1cf6a3dec90
Pavit Kiatkraipob <pavit.kiat@gmail.com>
2020-01-07 20:55:58 +0700

    Add first test

---
+++ test/app.test.js
@@ -0,0 +1,10 @@
+import getStateFromText from '../src/app.js'
+
+
+describe('getStateFromText function', () => {
+  test('it should return 0 if text in eventObject is not `พอ`', () => {
+    const text = 'อะไรก็ไม่รู้ อยากให้ช่วยจำหน่อย'
+    const state = getStateFromText(text)
+    expect(state).toBe(0)
+  })
+})
