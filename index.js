e12cfe8aeddb9a267a8865214b75816dacf8e005
Pavit Kiatkraipob <pavit.kiat@gmail.com>
2020-01-08 21:13:03 +0700

    move app logic to server/server.js

--- index.js
+++ index.js
@@ -1,22 +1,3 @@
-const express = require('express')
-const fs = require('fs')
-const middleware = require('@line/bot-sdk').middleware
-require('dotenv').config()
-
-const app = express()
-const port = 3000
-
-const config = {
-  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
-  channelSecret: process.env.CHANNEL_SECRET
-}
-
-app.post('/', middleware(config), (req, res) => {
-  fs.writeFile('latest_request.txt', JSON.stringify(req.body, null, 2), function (err) {
-    if (err) throw err;
-    console.log(JSON.stringify(req.body, null, 2));
-    res.json('OK')
-  });
-})
+const app = require('./server/server')
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
