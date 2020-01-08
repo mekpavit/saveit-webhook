2a8947d5d75947670feb469ab353a5410ffe5ec9
Pavit Kiatkraipob <pavit.kiat@gmail.com>
2020-01-08 21:13:40 +0700

    move app logic to server/server.js

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const fs = require('fs')
+const middleware = require('@line/bot-sdk').middleware
+require('dotenv').config()
+
+const app = express()
+const port = 3000
+
+const config = {
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.CHANNEL_SECRET
+}
+
+app.post('/', middleware(config), (req, res) => {
+  const eventObject = req.body
+  res.send(handleRequest(eventObject))
+})
+
+module.exports = app
