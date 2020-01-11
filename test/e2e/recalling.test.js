a9db879e737c0fd7be46f101bd3bd6afc266170f
Pavit Kiatkraipob <pavit.kiat@gmail.com>
2020-01-11 20:40:33 +0700

    Add e2e test of new feature

---
+++ test/e2e/recalling.test.js
@@ -0,0 +1,131 @@
+jest.mock('@line/bot-sdk', () => ({
+  middleware: (config) => (req, res, next) => next()
+}))
+
+import app from './../../server/server'
+const request = require('supertest')
+
+describe('Recalling messages', () => {
+
+  test('it should recall messages when input is `ขอข้อความที่1`', async () => {
+    const first_request = {
+      "events": [
+        {
+          "type": "message",
+          "replyToken": "aaaa",
+          "source": {
+            "userId": "bbbb",
+            "type": "user"
+          },
+          "timestamp": 1578194471425,
+          "mode": "active",
+          "message": {
+            "type": "text",
+            "id": "cccc",
+            "text": "ข้อความนี้ คือ สิ่งที่อยากให้จำ"
+          }
+        }
+      ],
+      "destination": "dddd"
+    }
+    await request(app).post('/').send(first_request)
+    const second_request = {
+      "events": [
+        {
+          "type": "message",
+          "replyToken": "aaaa",
+          "source": {
+            "userId": "bbbb",
+            "type": "user"
+          },
+          "timestamp": 1578194471425,
+          "mode": "active",
+          "message": {
+            "type": "text",
+            "id": "cccc",
+            "text": "ข้อความนี้ คือ อีกข้อความที่อยากให้จำ"
+          }
+        }
+      ],
+      "destination": "dddd"
+    }
+    await request(app).post('/').send(second_request)
+
+    const enough_request = {
+      "events": [
+        {
+          "type": "message",
+          "replyToken": "aaaa",
+          "source": {
+            "userId": "bbbb",
+            "type": "user"
+          },
+          "timestamp": 1578194471425,
+          "mode": "active",
+          "message": {
+            "type": "text",
+            "id": "cccc",
+            "text": "พอ"
+          }
+        }
+      ],
+      "destination": "dddd"
+    }
+    await request(app).post('/').send(enough_request)
+
+    const naming_request = {
+      "events": [
+        {
+          "type": "message",
+          "replyToken": "aaaa",
+          "source": {
+            "userId": "bbbb",
+            "type": "user"
+          },
+          "timestamp": 1578194471425,
+          "mode": "active",
+          "message": {
+            "type": "text",
+            "id": "cccc",
+            "text": "ชื่อข้อความที่1"
+          }
+        }
+      ],
+      "destination": "dddd"
+    }
+    await request(app).post('/').send(naming_request)
+
+    const recalling_request = {
+      "events": [
+        {
+          "type": "message",
+          "replyToken": "aaaa",
+          "source": {
+            "userId": "bbbb",
+            "type": "user"
+          },
+          "timestamp": 1578194471425,
+          "mode": "active",
+          "message": {
+            "type": "text",
+            "id": "cccc",
+            "text": "ขอข้อความที่1"
+          }
+        }
+      ],
+      "destination": "dddd"
+    }
+    const res = await request(app).post('/').send(recalling_request)
+    expect(req.body).toBe([
+      {
+        "type": "text",
+        "text": "ข้อความนี้ คือ สิ่งที่อยากให้จำ"
+      },
+      {
+        "type": "text",
+        "text": "ข้อความนี้ คือ อีกข้อความที่อยากให้จำ"
+      }
+    ])
+  })
+
+})
