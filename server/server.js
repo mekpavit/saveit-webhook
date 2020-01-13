5e22a08cc76fb5253e70947f5c33411fdc8e0a8e
Pavit Kiatkraipob <pavit.kiat@gmail.com>
2020-01-13 22:01:51 +0700

    Complete memorizing test

--- server/server.js
+++ server/server.js
@@ -16,13 +16,29 @@ const client = line.Client(config)
 app.use(line.middleware(config))
 app.use(bodyParser.json())
 
+const messageDatabase = {}
+
 app.post('/', (req, res) => {
+  
   const text = req.body.events[0].message.text
   const eventToken = req.body.events[0].replyToken
-  res.json(client.replyMessage(eventToken, {
-    "type": "text",
-    "text": "ถ้ามีข้อความที่อยากให้ผมจำอีก พิมพ์มาได้เลยนะครับ! ถ้าไม่มีแล้ว พิมพ์ว่า `พอ`"
-  }))
+  if (text.trim().indexOf('ชื่อ') === 0) {
+    res.json(client.replyMessage(eventToken, {
+      "type": "text",
+      "text": "ผมจำข้อความนี้ของพี่แล้วครับ ถ้าอยากให้ผมส่งข้อความให้ พิมพ์ `ขอ` ตามด้วยชื่อข้อความได้เลยครับ!"
+    }))
+  } else if (text.trim().indexOf('พอ') === 0) {
+    res.json(client.replyMessage(eventToken, {
+      "type": "text",
+      "text": "อยากให้ผมจำข้อความพวกนี้ด้วยชื่ออะไรครับ? พิมพ์ `ชื่อ` ตามด้วยชื่อที่ต้องการได้เลยครับ"
+    }))
+  } else {
+    res.json(client.replyMessage(eventToken, {
+      "type": "text",
+      "text": "ถ้ามีข้อความที่อยากให้ผมจำอีก พิมพ์มาได้เลยนะครับ! ถ้าไม่มีแล้ว พิมพ์ว่า `พอ`"
+    }))
+  }
+  
 })
 
 module.exports = app
