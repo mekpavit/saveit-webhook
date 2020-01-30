05be4231e822901123597dac359754b6d8b45a4a
Pavit Kiatkraipob <pavit.kiat@gmail.com>
2020-01-30 22:33:25 +0700

    Add SaveItRequest

---
+++ saveit/saveit-request.ts
@@ -0,0 +1,23 @@
+class SaveItRequest {
+
+  private _userId: string;
+  private _platformName: string;
+  private _messages: Array<Message>;
+  private _requestStatus: RequestStatus;
+
+  constructor(userId: string, platformName: string) {
+    this._userId = userId;
+    this._platformName = platformName;
+  }
+
+  addMessage(message: Message) {
+    this._messages.push(message);
+  }
+  
+  public getRequestStatus(): RequestStatus {
+    // do
+  }
+
+}
+
+enum RequestStatus {ADD, STOP, SAVE, RECALL}
