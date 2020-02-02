dc4d7b0444c51f659063a254236cbe838bdb96e4
Pavit Kiatkraipob <pavit.kiat@gmail.com>
2020-02-02 21:18:51 +0700

    Complete SaveItRequest

--- saveit/saveit-request.ts
+++ saveit/saveit-request.ts
@@ -1,25 +1,57 @@
 import { Message } from './message'
 
-class SaveItRequest {
+export class SaveItRequest {
 
   private _userId: string;
-  private _platformName: string;
   private _messages: Array<Message>;
+  private _messageName: string;
   private _requestStatus: RequestStatus;
+  private _platformCustomPayload: Object;
 
-  constructor(userId: string, platformName: string) {
-    this._userId = userId;
-    this._platformName = platformName;
+  constructor() {
+    this._messages = [];
   }
 
-  addMessage(message: Message) {
+  public addMessage(message: Message) {
     this._messages.push(message);
   }
+
+  public getMessage(): Array<Message> {
+    return this._messages;
+  }
+
+  public setUserId(userId: string): void {
+    this._userId = userId
+  }
+
+  public getUserId(): string {
+    return this._userId;
+  }
+
+  public setMessageName(messageName: string): void {
+    this._messageName = messageName;
+  }
+
+  public getMessageName(): string {
+    return this._messageName;
+  }
+
+  public setRequestStatus(requestStatus: RequestStatus): void {
+    this._requestStatus = requestStatus;
+  }
   
-  // public getRequestStatus(): RequestStatus {
-  //   // do
-  // }
+  public getRequestStatus(): RequestStatus {
+    return this._requestStatus;
+  }
+
+  public setPlatformCustomPayload(payload: Object): void {
+    this._platformCustomPayload = payload;
+  }
+
+  public getPlatformCustomPayload(): Object {
+    return this._platformCustomPayload;
+  }
 
 }
 
-enum RequestStatus {ADD, STOP, SAVE, RECALL}
+export enum RequestStatus {Add, Stop, Save, Recall}
