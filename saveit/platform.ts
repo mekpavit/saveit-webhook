6e08205ae1049361546868f24818ac3367a9b085
Pavit Kiatkraipob <pavit.kiat@gmail.com>
2020-02-02 21:17:35 +0700

    Add Platform API

--- saveit/platform.ts
+++ saveit/platform.ts
@@ -1,3 +1,9 @@
-interface Platform {
-  parseRequest(req: Object): SaveItRequest;
+import { SaveItRequest } from './saveit-request';
+import { SaveItResponse } from './saveit-response'
+
+export interface Platform {
+
+  parseRequest(req: Object): Promise<SaveItRequest>;
+  sendMessages(saveItResponse: SaveItResponse): Promise<boolean>;
+
 }
