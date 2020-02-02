78e33d0e0173bd279efa81d27b01e0431416932a
Pavit Kiatkraipob <pavit.kiat@gmail.com>
2020-02-02 21:17:26 +0700

    Add MessageDatabase API

--- saveit/database.ts
+++ saveit/database.ts
@@ -1,3 +1,7 @@
-interface Database {
+export interface MessageDatabase {
   
+  addMessage(messages: Array<Object>, userId: string): Promise<void>;
+  saveMessageName(messageName: string, userId: string): Promise<string>;
+  getMessages(messageName: string, userId: string): Promise<Array<Object>>;
+
 }
