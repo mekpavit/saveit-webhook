a9a55ed658348851f9222b4579964d8e3390d74a
Pavit Kiatkraipob <pavit.kiat@gmail.com>
2020-02-02 21:19:05 +0700

    Update exported modules

--- saveit/index.js
+++ saveit/index.js
@@ -1,1 +1,7 @@
-export { MessageType, Message, TextMessage, ImageMessage } from './message'
+export { MessageType, Message, TextMessage, ImageMessage } from './message';
+export { SaveItClient } from './saveit-client';
+export { Platform } from './platform';
+export { MessageDatabase } from './database';
+export { Storage } from './storage';
+export { SaveItRequest, RequestStatus } from './saveit-request';
+export { SaveItResponse } from './saveit-response'
