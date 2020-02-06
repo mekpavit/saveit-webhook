5b82cb041141b3ba2fca20d0b473e44e42d3e5d2
Pavit Kiatkraipob <pavit.kiat@gmail.com>
2020-02-06 22:02:50 +0700

    Add unit test

--- test/unit/message.test.ts
+++ test/unit/message.test.ts
@@ -1,4 +1,4 @@
-import { MessageType, TextMessage, ImageMessage } from '../../saveit'
+import { MessageType, TextMessage, ImageMessage, MessageParser } from '../../saveit'
 import { text } from 'body-parser';
 
 describe("TextMessage", () => {
@@ -19,6 +19,13 @@ describe("TextMessage", () => {
   })
 
   describe("equals", () => {
+
+    test('when input is null, it should return false', () => {
+      const textMessage = new TextMessage('a');
+      const that = null
+      const result = textMessage.equals(that);
+      expect(result).toBe(false);
+    })
     
     test('when input is itself, it should return true', () => {
       const textMessage = new TextMessage('a');
@@ -52,6 +59,11 @@ describe("TextMessage", () => {
 
   describe("fromJSON", () => {
 
+    test("when input is not valid, it should throw an error", () => {
+      const wrongMessageObject = {'type': 'abc', 'text': '123'}
+      expect(() => TextMessage.fromJSON(wrongMessageObject)).toThrow('Object must be {"type": "text", "text": string}')
+    })
+
     test("it should return TextMessage", () => {
 
       const expectedTextMessage = new TextMessage("test from json")
@@ -82,6 +94,13 @@ describe("ImageMessage", () => {
   })
   
   describe("equals", () => {
+
+    test('when input is null, it should return false', () => {
+      const imageMessage = new ImageMessage('www.image.com');
+      const that = null
+      const result = imageMessage.equals(that);
+      expect(result).toBe(false);
+    })
     
     test('when input is itself, it should return true', () => {
       const imageMessage = new ImageMessage('www.image.com');
@@ -115,12 +134,58 @@ describe("ImageMessage", () => {
 
   describe("fromJSON", () => {
 
-    test("it should return TextMessage", () => {
+    test("when input is not valid, it should throw an error", () => {
+      const wrongMessageObject = {'type': 'abc', 'imageUrl': '123'}
+      expect(() => ImageMessage.fromJSON(wrongMessageObject)).toThrow('Object must be {"type": "image", "imageUrl": string}')
+    })
 
-      const expectedImageMessage = new TextMessage("www.image.com")
-      const result = ImageMessage.fromJSON({"type": "text", "imageUrl": "www.image.com"});
+    test("it should return ImageMessage", () => {
+
+      const expectedImageMessage = new ImageMessage("www.image.com")
+      const result = ImageMessage.fromJSON({"type": "image", "imageUrl": "www.image.com"});
       expect(result).toEqual(expectedImageMessage);
 
+    })
+
+  })
+
+})
+
+
+describe('MessageParser', () => {
+
+  describe('parseObject', () => {
+
+    test('when input is null, it should throw error', () => {
+      expect(() => MessageParser.parseObject(null)).toThrow('Expect valid message JSON object but `null` was given');
+    })
+
+    test('when input does not have `type` attribute, it show throw error', () => {
+      const wrongMessageObject = {"text": "afdfdf"};
+      expect(() => MessageParser.parseObject(wrongMessageObject)).toThrow('Expect `type` attribute in the input object');
+    })
+
+    test('when input is TextMessage as JSON, it should return TextMessage', () => {
+      const expectedTextMessage: TextMessage = new TextMessage("aaaa");
+
+      const inputTextMessageJSON: Object = {"type": "text", "text": "aaaa"};
+      const result = MessageParser.parseObject(inputTextMessageJSON);
+      expect(result).toEqual(expectedTextMessage);
+
+    })
+
+    test('when input is ImageMessage as JSON, it should return ImageMessage', () => {
+      const expectedImageMessage: ImageMessage = new ImageMessage("www.image.com");
+
+      const inputImageMessage: Object = {"type": "image", "imageUrl": "www.image.com"};
+      const result = MessageParser.parseObject(inputImageMessage);
+      expect(result).toEqual(expectedImageMessage);
+
+    })
+
+    test('when input does not match any message type, it should throw an error', () => {
+      const wrongMessageObject: Object = {"type": "notValid"};
+      expect(() => MessageParser.parseObject(wrongMessageObject)).toThrow('The input object does not match any message type');
     })
 
   })
