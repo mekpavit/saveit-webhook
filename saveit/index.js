7c2b14520574cc4e0b6884d1511187b7562e2f5e
Pavit Kiatkraipob <pavit.kiat@gmail.com>
2020-02-08 18:57:58 +0700

    Add export

--- saveit/index.js
+++ saveit/index.js
@@ -1,6 +1,6 @@
 export { MessageType, Message, TextMessage, ImageMessage, MessageParser } from './message';
 export { SaveItClient } from './saveit-client';
-export { Platform } from './platform';
+export { Platform, LINEPlatform } from './platform';
 export { MessageDatabase } from './database';
 export { Storage } from './storage';
 export { SaveItRequest, RequestStatus } from './saveit-request';
