a48524ec67177bb16bc923ecbe3b9603fcfa3e43
Pavit Kiatkraipob <pavit.kiat@gmail.com>
2020-02-10 09:51:37 +0700

    Complete text message handling on LINEPlatform

--- saveit/platform.ts
+++ saveit/platform.ts
@@ -1,6 +1,9 @@
-import { SaveItRequest } from './saveit-request';
+import { SaveItRequest, RequestStatus } from './saveit-request';
 import { SaveItResponse } from './saveit-response';
 import * as line from '@line/bot-sdk';
+import { TextMessage, ImageMessage } from './message';
+import { Storage } from './storage';
+import sharp from 'sharp';
 
 export interface Platform {
 
@@ -10,29 +13,98 @@ export interface Platform {
 
 }
 
-export class LINEPlatform {
+export class LINEPlatform implements Platform {
 
   private _client: line.Client;
   private _channelSecret: string;
+  private _storage: Storage;
 
-  constructor(channelAccessToken: string, channelSecret: string) {
+  constructor(channelAccessToken: string, channelSecret: string, storage: Storage) {
     this._client = new line.Client({channelAccessToken, channelSecret});
     this._channelSecret = channelSecret;
+    this._storage = storage;
   }
 
-  public parseHTTPRequest(req: {headers: Object, body: string}): Promise<SaveItRequest> {
-    const saveItRequest = new SaveItRequest();
-    const requestBody: line.WebhookRequestBody = JSON.parse(req.body);
+  public parseHTTPRequest(req: {headers: Object, body: Object}): Promise<SaveItRequest> {
+    return new Promise<SaveItRequest>((resolve, reject) => {
+      const saveItRequest = new SaveItRequest();
+      // const requestBody: line.WebhookRequestBody = JSON.parse(req.body);
+      const requestBody = <line.WebhookRequestBody> req.body;
+      requestBody.events.forEach((event) => {
+        if (event.type === 'message') {
+          const messageEvent = <line.MessageEvent> event;
+          const message = messageEvent.message;
+          saveItRequest.setPlatformCustomPayload({'replyToken': messageEvent.replyToken});
+          saveItRequest.setUserId(event.source.userId);
+          if (message.type === 'text') {
+            if (message.text.trim().indexOf('ขอ') === 0) {
+              saveItRequest.setRequestStatus(RequestStatus.Recall);
+              saveItRequest.setMessageName(message.text.trim().replace('ขอ', ''))
+            } else if (message.text.trim().indexOf('ชื่อ') === 0) {
+              saveItRequest.setRequestStatus(RequestStatus.Save);
+              saveItRequest.setMessageName(message.text.trim().replace('ชื่อ', ''))
+            } else if (message.text.trim() === 'พอ') {
+              saveItRequest.setRequestStatus(RequestStatus.Stop);
+            } else {
+              saveItRequest.setRequestStatus(RequestStatus.Add);
+            }
+          } else {
+            saveItRequest.setRequestStatus(RequestStatus.Add);
+          }
+        }
+      })
+      if (saveItRequest.getRequestStatus() === RequestStatus.Add) {
+        requestBody.events.forEach( async (event) => {
+          if (event.type === 'message') {
+            const messageEvent = <line.MessageEvent> event;
+            const message = messageEvent.message;
+            if (message.type === 'text') {
+              saveItRequest.addMessage(new TextMessage(message.text));
+            }
+            if (message.type === 'image') {
+              const readableImage = await this._client.getMessageContent(message.id);
+              const imageUrl = await this._storage.uploadAndGetUrl(message.id, readableImage);
+              saveItRequest.addMessage(new ImageMessage(imageUrl));
+            }
+          }
+        })
+      }
+      
+      resolve(saveItRequest);
+    })
+    
 
   }
 
-  public validateHTTPRequest(req: {headers: Object, body: string}): void {
+  public validateHTTPRequest(req: {headers: {"x-line-signature": string}, body: string}): void {
     const signature = req.headers["x-line-signature"];
     if (!line.validateSignature(req.body, this._channelSecret, signature)) {
       throw new Error('signature validation failed');
     }
   }
 
-  private _
+  public sendMessages(saveItResponse: SaveItResponse): Promise<boolean> {
+
+    return new Promise<boolean>(async (resolve, reject) => {
+      const messages = new Array<line.Message>();
+      saveItResponse.getMessages().forEach((message) => {
+        const messageObject = <{type: string}> message.toJSON();
+        if (messageObject['type'] === 'text') {
+          messages.push(<line.TextMessage> messageObject)
+        }
+        if (messageObject['type'] === 'image') {
+          // const publicImageUrl = await this._storage.getSignedUrlFromFileName(messageObject['imageUrl']);
+          // sharp(publicImageUrl).resize(240, 240).png()
+        
+        }
+      });
+      const payload = <{replyToken: string}> saveItResponse.getPlatformCustomPayload()
+      const replyToken = payload['replyToken'];
+      console.log(messages);
+      const result = await this._client.replyMessage(replyToken, messages)
+      resolve(true)
+    })
+
+  }
 
 }
--- test/unit/platform.test.ts
+++ test/unit/platform.test.ts
@@ -1,11 +1,171 @@
-import { LINEPlatform } from './../../saveit';
+import { LINEPlatform, SaveItRequest, RequestStatus, Storage, Message, TextMessage, ImageMessage } from './../../saveit';
+import { Readable } from 'stream';
+
+class StubStorage implements Storage {
+
+  private _fileName: string;
+
+  constructor(fileName: string) {
+    this._fileName = fileName;
+  }
+
+  public uploadAndGetUrl(fileName: string, readable: Readable): Promise<string> {
+    return new Promise<string>((resolve, reject) => resolve(this._fileName));
+  }
+
+  public getSignedUrlFromFileName(fileName: string): Promise<string> {
+    return new Promise<string>((resolve, reject) => resolve(this._fileName));
+  }
+
+}
+
 
 describe('LINEPlatform', () => {
 
   describe('parseHTTPRequest', () => {
 
-    test('', () => {
+    test('it should parse userId', async () => {
+      const req = {
+        'headers': {
+          'x-line-signature': 'abcde'
+        },
+        'body': '{"events": [{"replyToken": "abcd1234", "source": {"userId": "12345"}, "type": "message", "message": {"type": "text", "text": "please remember"}}]}'
+      };
+
+      const storage = new StubStorage('imageid.jpeg');
+      const platform = new LINEPlatform("abc", "abc", storage);
+      const saveItRequest: SaveItRequest  = await platform.parseHTTPRequest(req);
+      expect(saveItRequest.getUserId()).toEqual('12345');
+    })
+
+    test('it should parse replyToken', async () => {
+      const req = {
+        'headers': {
+          'x-line-signature': 'abcde'
+        },
+        'body': '{"events": [{"replyToken": "abcd1234", "source": {"userId": "12345"}, "type": "message", "message": {"type": "text", "text": "please remember"}}]}'
+      };
+
+      const storage = new StubStorage('imageid.jpeg');
+      const platform = new LINEPlatform("abc", "abc", storage);
+      const saveItRequest: SaveItRequest  = await platform.parseHTTPRequest(req);
+      expect(saveItRequest.getPlatformCustomPayload()).toEqual({"replyToken": "abcd1234"});
+    })
+
+    test('when there is no word `พอ`, `ชื่อ`, `ขอ` in the req, it should parse RequestStatus.ADD', async () => {
+
+      const req = {
+        'headers': {
+          'x-line-signature': 'abcde'
+        },
+        'body': '{"events": [{"replyToken": "abcd1234", "source": {"userId": "12345"}, "type": "message", "message": {"type": "text", "text": "please remember"}}]}'
+      };
+
+      const storage = new StubStorage('imageid.jpeg');
+      const platform = new LINEPlatform("abc", "abc", storage);
+      const saveItRequest: SaveItRequest  = await platform.parseHTTPRequest(req);
+      expect(saveItRequest.getRequestStatus()).toEqual(RequestStatus.Add);
       
+    })
+
+    test('when there is a word `พอ` in the req, it should parse RequestStatus.STOP', async () => {
+
+      const req = {
+        'headers': {
+          'x-line-signature': 'abcde'
+        },
+        'body': '{"events": [{"replyToken": "abcd1234", "source": {"userId": "12345"}, "type": "message", "message": {"type": "text", "text": "พอ"}}]}'
+      };
+
+      const storage = new StubStorage('imageid.jpeg');
+      const platform = new LINEPlatform("abc", "abc", storage);
+      const saveItRequest: SaveItRequest  = await platform.parseHTTPRequest(req);
+      expect(saveItRequest.getRequestStatus()).toEqual(RequestStatus.Stop);
+
+    })
+
+    test('when there is a word `ชื่อ` in the req, it should parse RequestStatus.Save', async () => {
+
+      const req = {
+        'headers': {
+          'x-line-signature': 'abcde'
+        },
+        'body': '{"events": [{"replyToken": "abcd1234", "source": {"userId": "12345"}, "type": "message", "message": {"type": "text", "text": "ชื่อข้อความหนึ่ง"}}]}'
+      };
+
+      const storage = new StubStorage('imageid.jpeg');
+      const platform = new LINEPlatform("abc", "abc", storage);
+      const saveItRequest: SaveItRequest  = await platform.parseHTTPRequest(req);
+      expect(saveItRequest.getRequestStatus()).toEqual(RequestStatus.Save);
+
+    })
+
+    test('when there is a word `ชื่อ` in the req, it should parse messageName', async () => {
+
+      const req = {
+        'headers': {
+          'x-line-signature': 'abcde'
+        },
+        'body': '{"events": [{"replyToken": "abcd1234", "source": {"userId": "12345"}, "type": "message", "message": {"type": "text", "text": "ชื่อข้อความหนึ่ง"}}]}'
+      };
+
+      const storage = new StubStorage('imageid.jpeg');
+      const platform = new LINEPlatform("abc", "abc", storage);
+      const saveItRequest: SaveItRequest  = await platform.parseHTTPRequest(req);
+      expect(saveItRequest.getMessageName()).toEqual('ข้อความหนึ่ง');
+
+    })
+
+    test('when there is a word `ขอ` in the req, it should parse RequestStatus.Recall', async () => {
+
+      const req = {
+        'headers': {
+          'x-line-signature': 'abcde'
+        },
+        'body': '{"events": [{"replyToken": "abcd1234", "source": {"userId": "12345"}, "type": "message", "message": {"type": "text", "text": "ขอข้อความหนึ่ง"}}]}'
+      };
+
+      const storage = new StubStorage('imageid.jpeg');
+      const platform = new LINEPlatform("abc", "abc", storage);
+      const saveItRequest: SaveItRequest  = await platform.parseHTTPRequest(req);
+      expect(saveItRequest.getRequestStatus()).toEqual(RequestStatus.Recall);
+
+    })
+
+    test('when there is a word `ขอ` in the req, it should parse messageName', async () => {
+
+      const req = {
+        'headers': {
+          'x-line-signature': 'abcde'
+        },
+        'body': '{"events": [{"replyToken": "abcd1234", "source": {"userId": "12345"}, "type": "message", "message": {"type": "text", "text": "ชื่อข้อความหนึ่ง"}}]}'
+      };
+
+      const storage = new StubStorage('imageid.jpeg');
+      const platform = new LINEPlatform("abc", "abc", storage);
+      const saveItRequest: SaveItRequest  = await platform.parseHTTPRequest(req);
+      expect(saveItRequest.getMessageName()).toEqual('ข้อความหนึ่ง');
+
+    })
+
+    test('when there are more than one event, it should keep all messages', async () => {
+
+      const req = {
+        'headers': {
+          'x-line-signature': 'abcde'
+        },
+        'body': '{"events": [{"replyToken": "abcd1234", "source": {"userId": "12345"}, "type": "message", "message": {"type": "text", "text": "remember 1"}}, {"replyToken": "abcd1234", "source": {"userId": "12345"}, "type": "message", "message": {"type": "text", "text": "remember 2"}}]}'
+      };
+
+      const expectedMessages = [
+        new TextMessage('remember 1'), new TextMessage('remember 2')
+      ]
+
+      const storage = new StubStorage('imageid.jpeg');
+      const platform = new LINEPlatform("abc", "abc", storage);
+      const saveItRequest: SaveItRequest  = await platform.parseHTTPRequest(req);
+      expect(saveItRequest.getMessage()).toStrictEqual<Array<Message>>(expectedMessages);
+
     })
 
   })
