c9592b80a04498375d5b48760def6ed7c8f90d35
Pavit Kiatkraipob <pavit.kiat@gmail.com>
2020-02-10 09:52:03 +0700

    Remove files

--- saveit/index.js
+++
@@ -1,7 +0,0 @@
-export { MessageType, Message, TextMessage, ImageMessage, MessageParser } from './message';
-export { SaveItClient } from './saveit-client';
-export { Platform, LINEPlatform } from './platform';
-export { MessageDatabase } from './database';
-export { Storage } from './storage';
-export { SaveItRequest, RequestStatus } from './saveit-request';
-export { SaveItResponse } from './saveit-response'
