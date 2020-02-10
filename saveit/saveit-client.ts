6a19828b5abe296a6cebc64b9fb4aafc117983e4
Pavit Kiatkraipob <pavit.kiat@gmail.com>
2020-02-10 09:53:39 +0700

    temporary remove HTTP request validation

--- saveit/saveit-client.ts
+++ saveit/saveit-client.ts
@@ -15,7 +15,6 @@ export class SaveItClient {
   }
 
   public async handleHTTPRequest(req: {headers: Object, body: string}): Promise<boolean> {
-    this._platform.validateHTTPRequest(req);
     const saveItRequest: SaveItRequest = await this._platform.parseHTTPRequest(req);
     return await this.handleSaveItRequest(saveItRequest);
 
@@ -25,7 +24,8 @@ export class SaveItClient {
     const requestStatus = saveItRequest.getRequestStatus();
     if (requestStatus === RequestStatus.Add) {
       try {
-        await this._db.addMessage(saveItRequest.getMessage(), saveItRequest.getUserId());
+        const messageObject = saveItRequest.getMessage().map((message) => message.toJSON())
+        await this._db.addMessage(messageObject, saveItRequest.getUserId());
         const saveItResponse = new SaveItResponse();
         saveItResponse.addMessage(new TextMessage('ถ้ามีข้อความที่อยากให้ผมจำอีก พิมพ์มาได้เลยนะครับ! ถ้าไม่มีแล้ว พิมพ์ว่า `พอ`'));
         saveItResponse.setPlatformCustomPayload(saveItRequest.getPlatformCustomPayload());
@@ -43,6 +43,7 @@ export class SaveItClient {
       const userId = saveItRequest.getUserId()
       const savedMessageName = await this._db.saveMessageName(messageName, userId);
       const saveitResponse = new SaveItResponse();
+      saveitResponse.setPlatformCustomPayload(saveItRequest.getPlatformCustomPayload());
       saveitResponse.addMessage(new TextMessage('ผมจำข้อความนี้ของพี่แล้วครับ ถ้าอยากให้ผมส่งข้อความให้ พิมพ์ `ขอ` ตามด้วยชื่อข้อความได้เลยครับ! เช่น `ขอ' + savedMessageName + '`'));
       this._platform.sendMessages(saveitResponse);
     } else if (requestStatus === RequestStatus.Recall) {
@@ -50,6 +51,7 @@ export class SaveItClient {
       const userId = saveItRequest.getUserId();
       const messages = await this._db.getMessages(messageName, userId);
       const saveitResponse = new SaveItResponse();
+      saveitResponse.setPlatformCustomPayload(saveItRequest.getPlatformCustomPayload());
       messages.forEach((msg) => {
         const messageObject = MessageParser.parseObject(msg);
         saveitResponse.addMessage(messageObject);
